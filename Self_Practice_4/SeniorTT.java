4bb12035f59d0c5704d577ccf07ada24613cd2b5
Rafi Amin <mohammadrafi2119@gmail.com>
2026-05-16 18:04:05 +0600

    Exam Question added here.

---
+++ Self_Practice_4/SeniorTT.java
@@ -0,0 +1,30 @@
+class Person {
+    String name;
+    int age;
+    String phoneNumber;
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    void show() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        System.out.println("Phone: " + phoneNumber);
+       
+    }
+}
+public class SeniorTT {
+    public static void main(String[] args) {
+        Person p1 = new Person("Rafi", 20);
+        Person p2 = new Person("Sami", 22, "01712345678");
+        p1.show();
+        p2.show();
+    }
+}
